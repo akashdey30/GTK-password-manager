fbcd0544ec19b96150581ae980de9737ac53a5a0
Akash Dey <240230@ku.ac.bd>
2026-05-05 10:31:48 +0600

    Delete GTK_Password_Manager directory

--- GTK_Password_Manager/include/security/backup.h
+++
@@ -1,9 +0,0 @@
-#ifndef BACKUP_H
-#define BACKUP_H
-
-#include "global_types.h"
-
-AppResult backup_export(const char *db_path, const char *dest_path);
-AppResult backup_import(const char *src_path, const char *db_path);
-
-#endif
